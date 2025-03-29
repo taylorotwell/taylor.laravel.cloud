725b14ec41020f143a849f462988f3cb95e676fa
Taylor Otwell <taylor@laravel.com>
2025-03-29 13:37:33 -0500

    wip

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => ['default', 'emails'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dispatch', function () {
+    TestJob::dispatch();
+
+    return 'OK';
 });
